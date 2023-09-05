20412d6570d5b556fe0641a8f0f74ce530f517b2
zealzel <zealzel@gmail.com>
2023-09-06 00:24:38 +0800

    Fix bug in navigation_keepout.launch.py

--- linorobot2/linorobot2_navigation/launch/navigation_keepout.launch.py
+++ linorobot2/linorobot2_navigation/launch/navigation_keepout.launch.py
@@ -1,18 +1,3 @@
-# Copyright (c) 2021 Juan Miguel Jimeno
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import os
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
@@ -33,6 +18,20 @@ def get_path(package_name, subpaths):
 
 def generate_launch_description():
     package_name = "linorobot2_navigation"
+
+    nav2_launch_path = get_path("nav2_bringup", ["launch", "bringup_launch.py"])
+    costmap_filter_info_launch_path = get_path(package_name, ["launch", "costmap_filter_info.launch.py"])
+    rviz_config_path = get_path(package_name, ["rviz", "linorobot2_navigation.rviz"])
+    default_map_path = get_path(package_name, ["maps", f"{MAP_NAME}.yaml"])
+
+    use_sim_arg = DeclareLaunchArgument(
+        name="sim",
+        default_value="false",
+        description="Enable use_sime_time to true",
+    )
+    use_rviz_arg = DeclareLaunchArgument(
+        name="rviz", default_value="false", description="Run rviz"
+    )
     params_arg = DeclareLaunchArgument(
         "params_file",
         default_value=get_path(package_name, ["config", "navigation_keepout.yaml"]),
@@ -49,53 +48,25 @@ def generate_launch_description():
         "mask",
         description="mask file for keepout layer",
     )
-    nav2_launch_path = get_path("nav2_bringup", ["launch", "bringup_launch.py"])
-    costmap_filter_info_launch_path = get_path(package_name, ["launch", "costmap_filter_info.launch.py"])
-    rviz_config_path = get_path(package_name, ["rviz", "linorobot2_navigation.rviz"])
-    default_map_path = get_path(package_name, ["maps", f"{MAP_NAME}.yaml"])
-
-    use_sim_arg = (
-        DeclareLaunchArgument(
-            name="sim",
-            default_value="false",
-            description="Enable use_sime_time to true",
-        ),
+    map_arg = DeclareLaunchArgument(
+        name="map",
+        default_value=default_map_path,
+        description="Navigation map path",
     )
-    use_rviz_arg = (
-        DeclareLaunchArgument(
-            name="rviz", default_value="false", description="Run rviz"
-        ),
+    costmap_filter_info = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(costmap_filter_info_launch_path),
+        launch_arguments={
+            "params_file": LaunchConfiguration("keepout_params_file"),
+            "mask": LaunchConfiguration("mask"),
+        }.items(),
     )
-    map_arg = (
-        DeclareLaunchArgument(
-            name="map",
-            default_value=default_map_path,
-            description="Navigation map path",
-        ),
-    )
-
-    # ros2 launch nav2_costmap_filters_demo costmap_filter_info.launch.py \
-    # params_file:=$HOME/simulations/src/navigation2_tutorials/nav2_costmap_filters_demo/params/keepout_params.yaml \
-    # mask:=$HOME/maps/keepout_mask.yaml
-
-    costmap_filter_info = (
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(costmap_filter_info_launch_path),
-            launch_arguments={
-                "params_file": LaunchConfiguration("keepout_params_file"),
-                "mask": LaunchConfiguration("mask"),
-            }.items(),
-        ),
-    )
-    nav2_bringup = (
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(nav2_launch_path),
-            launch_arguments={
-                "map": LaunchConfiguration("map"),
-                "use_sim_time": LaunchConfiguration("sim"),
-                "params_file": LaunchConfiguration("params_file"),
-            }.items(),
-        ),
+    nav2_bringup = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(nav2_launch_path),
+        launch_arguments={
+            "map": LaunchConfiguration("map"),
+            "use_sim_time": LaunchConfiguration("sim"),
+            "params_file": LaunchConfiguration("params_file"),
+        }.items()
     )
     rviz = Node(
         package="rviz2",
