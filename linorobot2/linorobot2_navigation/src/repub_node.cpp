8fb5bceedcb2887162b68463666ef9f10b567164
zealzel <zealzel@gmail.com>
2024-02-17 09:36:10 +0800

    Modify repub_node.cpp to fix rangesensorlayer local_costmap not clear problem.

--- linorobot2/linorobot2_navigation/src/repub_node.cpp
+++ linorobot2/linorobot2_navigation/src/repub_node.cpp
@@ -15,6 +15,16 @@ class RepubNode : public rclcpp::Node {
             "/range2/data", 10,
             std::bind(&RepubNode::range2_callback, this, std::placeholders::_1));
 
+        pub_range3_sensor_ = this->create_publisher<sensor_msgs::msg::Range>("/range3_sensor", 10);
+        sub_range3_ = this->create_subscription<sensor_msgs::msg::Range>(
+            "/range3/data", 10,
+            std::bind(&RepubNode::range3_callback, this, std::placeholders::_1));
+
+        pub_range4_sensor_ = this->create_publisher<sensor_msgs::msg::Range>("/range4_sensor", 10);
+        sub_range4_ = this->create_subscription<sensor_msgs::msg::Range>(
+            "/range4/data", 10,
+            std::bind(&RepubNode::range4_callback, this, std::placeholders::_1));
+
         pub_cmd_vel_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
         sub_cmd_vel_ = this->create_subscription<geometry_msgs::msg::Twist>(
             "/cmd_vel", 10, std::bind(&RepubNode::cmd_vel_callback, this, std::placeholders::_1));
@@ -26,11 +36,30 @@ class RepubNode : public rclcpp::Node {
   private:
     void range1_callback(const sensor_msgs::msg::Range::SharedPtr msg) {
         last_range1_msg_ = *msg;
+        // fix the costmap-not-clear problem. ref: https://blog.csdn.net/xiekaikaibing/article/details/80096033
+        if (last_range1_msg_.range > last_range1_msg_.max_range)
+            last_range1_msg_.range = last_range1_msg_.max_range;
         msg_ready_ = true;
     }
 
     void range2_callback(const sensor_msgs::msg::Range::SharedPtr msg) {
         last_range2_msg_ = *msg;
+        if (last_range2_msg_.range > last_range2_msg_.max_range)
+            last_range2_msg_.range = last_range2_msg_.max_range;
+        msg_ready_ = true;
+    }
+
+    void range3_callback(const sensor_msgs::msg::Range::SharedPtr msg) {
+        last_range3_msg_ = *msg;
+        if (last_range3_msg_.range > last_range3_msg_.max_range)
+            last_range3_msg_.range = last_range3_msg_.max_range;
+        msg_ready_ = true;
+    }
+
+    void range4_callback(const sensor_msgs::msg::Range::SharedPtr msg) {
+        last_range4_msg_ = *msg;
+        if (last_range4_msg_.range > last_range4_msg_.max_range)
+            last_range4_msg_.range = last_range4_msg_.max_range;
         msg_ready_ = true;
     }
 
@@ -40,8 +69,7 @@ class RepubNode : public rclcpp::Node {
     }
 
     void timer_callback() {
-        if (msg_ready_) {
-            pub_range1_sensor_->publish(last_range1_msg_);
+        if (msg_ready_) { pub_range1_sensor_->publish(last_range1_msg_);
             pub_range2_sensor_->publish(last_range2_msg_);
             pub_cmd_vel_->publish(last_cmd_vel_msg_);
             msg_ready_ = false;
@@ -54,12 +82,20 @@ class RepubNode : public rclcpp::Node {
     rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr pub_range2_sensor_;
     rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr sub_range2_;
 
+    rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr pub_range3_sensor_;
+    rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr sub_range3_;
+
+    rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr pub_range4_sensor_;
+    rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr sub_range4_;
+
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_cmd_vel_;
     rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr sub_cmd_vel_;
 
     rclcpp::TimerBase::SharedPtr timer_;
     sensor_msgs::msg::Range last_range1_msg_;
     sensor_msgs::msg::Range last_range2_msg_;
+    sensor_msgs::msg::Range last_range3_msg_;
+    sensor_msgs::msg::Range last_range4_msg_;
     geometry_msgs::msg::Twist last_cmd_vel_msg_;
     bool msg_ready_;
 };
