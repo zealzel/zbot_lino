7617da5edf2c3b9b6c5ee1d1c2c2845a75ff57da
zealzel <zealzel@gmail.com>
2024-08-13 16:28:42 +0800

    Add launch_nav.launch.py. Define world-map relationship to avoid directly providing map arugment.

---
+++ linorobot2/linorobot2_navigation/launch/launch_nav.launch.py
@@ -0,0 +1,153 @@
+import os
+from launch import LaunchDescription
+from ament_index_python.packages import get_package_share_directory
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    SetEnvironmentVariable,
+    OpaqueFunction,
+    SetLaunchConfiguration,
+)
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+
+
+def get_path(package_name, subpaths):
+    return PathJoinSubstitution([FindPackageShare(package_name)] + subpaths)
+
+
+def include_launch_description(launch_path, **kwargs):
+    launch_args = kwargs.pop("launch_arguments", {})
+    return IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(launch_path),
+        launch_arguments=launch_args.items(),
+        **kwargs,
+    )
+
+
+WORLD_MAP = {
+    "turtlebot3_world": "turtlebot3_world.yaml",
+    "room_with_tags": "room_with_tags.yaml",
+    "obstacles": "obstacles.yaml",
+    "turtlebot3_house": "xxx",
+}
+
+
+def generate_launch_description():
+    package_name = "linorobot2_navigation"
+    fitrobot_install_path = get_package_share_directory("fitrobot")
+
+    # simulation only: 2wd|4wd|macanum|zbotlinolong
+    # real robot: zbotlino(use rplidar)|zbotlinosick1
+    # robot_base = os.getenv('LINOROBOT2_BASE', 'zbotlino')
+    # robot_base = os.getenv('LINOROBOT2_BASE', 'zbotlinosick2')
+
+    robot_base = os.getenv("LINOROBOT2_BASE", "zbotlino2")
+    package_name = "linorobot2_navigation"
+
+    # the footprint of both is the same
+    if robot_base in ["zbotlino", "zbotlinosick1"]:
+        robot_base = "zbotlino"
+
+    default_worldname = "turtlebot3_world"
+
+    def worldname_to_map(context):
+        if "sim" in context.launch_configurations:
+            worldname = context.launch_configurations.get(
+                "worldname", default_worldname
+            )
+            print("worldname_to_map", worldname)
+            mappath = os.path.join(
+                fitrobot_install_path,
+                "maps/sim",
+                f"{WORLD_MAP[worldname]}",
+            )
+            print("mappath", mappath)
+            return [SetLaunchConfiguration("map", mappath)]
+
+    map_sim_arg = OpaqueFunction(function=worldname_to_map)
+
+    # default_map_path = get_path("fitrobot", ["maps", "office_res002_0914.yaml"])
+    default_map_path = get_path("fitrobot", ["maps", "lino2_office_20240129.yaml"])
+
+    # default_map_path_sim = get_path(package_name, ["maps", "turtlebot3_world.yaml"])
+
+    params_file_path = get_path(package_name, ["config", robot_base, "navigation.yaml"])
+    nav2_launch_path = get_path("nav2_bringup", ["launch", "bringup_launch.py"])
+    # rviz_config_path = get_path("nav2_bringup", ["rviz", "nav2_default_view.rviz"])
+    # rviz_config_path = "/home/zealzel/.rviz2/nav2_camera.rviz"
+
+    use_sim_arg = DeclareLaunchArgument(
+        name="sim",
+        default_value="false",
+        description="Enable use_sime_time to true",
+    )
+    use_rviz_arg = DeclareLaunchArgument(
+        name="rviz", default_value="false", description="Run rviz"
+    )
+    rviz_config_arg = DeclareLaunchArgument(
+        "rviz_config",
+        default_value=get_path("nav2_bringup", ["rviz", "nav2_default_view.rviz"]),
+        description=("Full path to the ROS2 rviz config file"),
+    )
+    params_arg = DeclareLaunchArgument(
+        "params_file",
+        default_value=params_file_path,
+        description=(
+            "Full path to the ROS2 parameters file to use for all launched nodes"
+        ),
+    )
+    map_arg = DeclareLaunchArgument(
+        name="map",
+        default_value=default_map_path,
+        description="Navigation map path",
+        condition=UnlessCondition(LaunchConfiguration("sim")),
+    )
+    # map_sim_arg = DeclareLaunchArgument(
+    #     name="map",
+    #     default_value=default_map_path_sim,
+    #     description="Navigation map path",
+    #     condition=IfCondition(LaunchConfiguration("sim")),
+    # )
+    #
+    nav2_bringup = include_launch_description(
+        nav2_launch_path,
+        launch_arguments={
+            "map": LaunchConfiguration("map"),
+            "use_sim_time": LaunchConfiguration("sim"),
+            "params_file": LaunchConfiguration("params_file"),
+        },
+    )
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        # arguments=["-d", rviz_config_path],
+        arguments=["-d", LaunchConfiguration("rviz_config")],
+        condition=IfCondition(LaunchConfiguration("rviz")),
+        parameters=[{"use_sim_time": LaunchConfiguration("sim")}],
+    )
+    # Temporary node to republish the range1/data & range2/data to range1_sensor & range2_sensor
+    uros_repub = Node(
+        package=package_name,
+        executable="repub_node",
+        name="repub_node",
+        output="screen",
+    )
+    return LaunchDescription(
+        [
+            use_sim_arg,
+            use_rviz_arg,
+            rviz_config_arg,
+            params_arg,
+            map_arg,
+            map_sim_arg,
+            nav2_bringup,
+            rviz,
+            uros_repub,
+        ]
+    )
---
+++ linorobot2/linorobot2_navigation/readme.md
@@ -0,0 +1,17 @@
+# linorobot2_navigation
+
+## launch_nav
+
+This launch file is for single robot.
+
+Once you provide the worldname argument, the map will be decided.
+
+```bash
+ros2 launch linorobot2_navigation launch_nav.launch.py sim:=true rviz:=true worldname:=turtlebot3_house
+```
+
+Currently, the world-map relationships is as follows. This maps are in fitrobot/maps/sim directory
+
+- turtlebot3_world: turtlebot3_world.yaml
+- room_with_tags: room_with_tags.yaml
+- obstacles: obstacles.yaml
